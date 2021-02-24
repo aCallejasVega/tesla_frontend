f091f2493b632557d2ecb891a2b559595f33e555
DESKTOP-2P3AEAE\Erika <erisalvat@hotmail.com>
2021-02-23 20:23:36 -0400

    Se sube adicion de inputs en MontosUnitarios

--- src/service/Recaudaciones/PaymentDebts.service.js
+++ src/service/Recaudaciones/PaymentDebts.service.js
@@ -27,7 +27,7 @@ export default {
   cargarServicioDeudas(entidadId,codigoCliente) {
     return Api().get(`api/entidades/${entidadId}/clientes/${codigoCliente}/deudas`, this.headersConfig());
   },
-  cobrarDeudas(cliente, comprobanteAllinOne, metodoPagoId) {
-    return Api().post(`api/cobros/${comprobanteAllinOne}/${metodoPagoId}`, cliente, this.headersConfig());
+  cobrarDeudas(cliente, metodoPagoId) {
+    return Api().post(`api/cobros/${metodoPagoId}`, cliente, this.headersConfig());
   },
 };
